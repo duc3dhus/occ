e78335214c3c0026e6250249b1c4dfaf01ddda6c
duc3dhus <dodongduc@hus.edu.vn>
2013-10-15 23:00:44 +0700

    thay doi db

--- db/occ.sql
+++ db/occ.sql
@@ -1,111 +1,154 @@
--- phpMyAdmin SQL Dump
--- version 3.3.9
--- http://www.phpmyadmin.net
+CREATE DATABASE  IF NOT EXISTS `occ` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `occ`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
 --
--- Host: localhost
--- Generation Time: Oct 15, 2013 at 08:02 PM
--- Server version: 5.5.8
--- PHP Version: 5.3.5
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
+-- Host: 127.0.0.1    Database: occ
+-- ------------------------------------------------------
+-- Server version	5.5.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Database: `occ`
+-- Table structure for table `customer`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `customer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `customer` (
+  `customerId` int(11) NOT NULL AUTO_INCREMENT,
+  `customerName` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`customerId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `anh`
+-- Dumping data for table `customer`
 --
 
-CREATE TABLE IF NOT EXISTS `anh` (
+LOCK TABLES `customer` WRITE;
+/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `image`
+--
+
+DROP TABLE IF EXISTS `image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `image` (
+  `imageId` int(11) NOT NULL,
+  `imageName` varchar(200) NOT NULL,
+  `imageUrl` varchar(200) DEFAULT NULL,
+  `createDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`imageId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `image`
+--
+
+LOCK TABLES `image` WRITE;
+/*!40000 ALTER TABLE `image` DISABLE KEYS */;
+/*!40000 ALTER TABLE `image` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `productId` int(11) NOT NULL AUTO_INCREMENT,
+  `productName` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `service` int(11) NOT NULL,
+  `customer` int(11) NOT NULL,
+  PRIMARY KEY (`productId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product_image`
+--
+
+DROP TABLE IF EXISTS `product_image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product_image` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tenanh` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `productId` int(11) NOT NULL,
+  `imageId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pk_imageId_idx` (`imageId`),
+  KEY `pk_productId_idx` (`productId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `anh`
+-- Dumping data for table `product_image`
 --
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `anhsanpham`
---
-
-CREATE TABLE IF NOT EXISTS `anhsanpham` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sanphamID` int(11) NOT NULL,
-  `anhID` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+LOCK TABLES `product_image` WRITE;
+/*!40000 ALTER TABLE `product_image` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product_image` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Dumping data for table `anhsanpham`
+-- Table structure for table `service`
 --
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `dichvu`
---
-
-CREATE TABLE IF NOT EXISTS `dichvu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tendichvu` varchar(200) NOT NULL,
-  `mota` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+DROP TABLE IF EXISTS `service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `service` (
+  `serviceId` int(11) NOT NULL AUTO_INCREMENT,
+  `serviceName` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`serviceId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `dichvu`
+-- Dumping data for table `service`
 --
 
+LOCK TABLES `service` WRITE;
+/*!40000 ALTER TABLE `service` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- --------------------------------------------------------
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
---
--- Table structure for table `khachhang`
---
-
-CREATE TABLE IF NOT EXISTS `khachhang` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tenkhachhang` varchar(200) NOT NULL,
-  `mota` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `khachhang`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `sanpham`
---
-
-CREATE TABLE IF NOT EXISTS `sanpham` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tensanpham` varchar(200) NOT NULL,
-  `mota` text NOT NULL,
-  `dichvu` int(11) NOT NULL,
-  `khachhang` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `sanpham`
---
-
+-- Dump completed on 2013-10-15 22:59:52
